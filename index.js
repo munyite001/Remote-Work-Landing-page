05b67647c4756db515735e15111eb44697590870
munyite001 <emunyite@gmail.com>
2022-08-06 19:41:57 +0300

    Added Icons to mobile design

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ const toggle2 = document.getElementById('toggle2');
 const nested_list1 = document.getElementById('nested-list1');
 const nested_list2 = document.getElementById('nested-list2');
 
-const icon = document.querySelector('.icon');
+//Up and down icons
+const uparrow1 = document.getElementById('up-arrow1');
+const downarrow1 = document.getElementById('down-arrow1');
+const uparrow2 = document.getElementById('up-arrow2');
+const downarrow2 = document.getElementById('down-arrow2');  
+
+
 
 //  Show and Hide Sidebar
 btn.addEventListener ('click',() => {
@@ -31,10 +37,14 @@ toggle1.addEventListener('click', () => {
     if (nested_list1.classList.contains('show-nested-list')) 
     {
         nested_list1.classList.remove('show-nested-list');
+        uparrow1.classList.add('hide-icon');
+        downarrow1.classList.remove('hide-icon');
     }
     else
     {
         nested_list1.classList.add('show-nested-list');
+        uparrow1.classList.remove('hide-icon');
+        downarrow1.classList.add('hide-icon');
     }
 });
 
@@ -42,9 +52,13 @@ toggle2.addEventListener('click', () => {
     if (nested_list2.classList.contains('show-nested-list')) 
     {
         nested_list2.classList.remove('show-nested-list');
+        uparrow2.classList.add('hide-icon');
+        downarrow2.classList.remove('hide-icon');
     }
     else
     {
         nested_list2.classList.add('show-nested-list');
+        uparrow2.classList.remove('hide-icon');
+        downarrow2.classList.add('hide-icon');
     }
 });
