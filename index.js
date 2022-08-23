78edfb57a07cd8a54cd17e918a0fb7f71c6be031
munyite001 <emunyite@gmail.com>
2022-08-23 18:05:35 +0300

    Refactored some of the code for easier readability

--- index.js
+++ index.js
@@ -1,113 +1,39 @@
-//  Blurred Focus
-const body = document.querySelector('body');
-
-
 //  SIDEBAR
 const btn = document.querySelector('.toggle-btn');
 const close_btn = document.querySelector('.close-btn');
 const sidebar = document.querySelector('.sidebar');
 
-// Toggle Nested list Mobile
-const toggle_features_mobile = document.getElementById('toggle-features-mobile');
-const toggle_company_mobile = document.getElementById('toggle-company-mobile');
-const nested_list_features_mobile = document.getElementById('nested_list_features_mobile');
-const nested_list_company_mobile = document.getElementById('nested_list_company_mobile');
-
-
-// Toggle Nested list Desktop
-const toggle_features_desktop = document.getElementById('toggle-features-desktop');
-const toggle_company_desktop = document.getElementById('toggle-company-desktop');
-const nested_list_features_desktop= document.getElementById('nested_list_features_desktop');
-const nested_list_company_desktop = document.getElementById('nested_list_company_desktop');
-
-
-//  Up and down icons Mobile
-const uparrow1 = document.getElementById('up-arrow1');
-const downarrow1 = document.getElementById('down-arrow1');
-const uparrow2 = document.getElementById('up-arrow2');
-const downarrow2 = document.getElementById('down-arrow2');  
-
-// Up and down icons desktop
-const updesktop1 = document.getElementById('d-up-f');
-const downdesktop1 = document.getElementById('d-down-f');
-const updesktop2 = document.getElementById('d-up-c');
-const downdesktop2 = document.getElementById('d-down-c');  
-
-
 //  Show and Hide Sidebar
 btn.addEventListener ('click',() => {
-    body.classList.add('blurred-focus');
     sidebar.classList.add('show-sidebar');
 });
 
 close_btn.addEventListener('click',() => {
-    body.classList.remove('blurred-focus');
     sidebar.classList.remove('show-sidebar');
 });
 
-//  Show and hide nested list
-toggle_features_mobile.addEventListener('click', () => {
-    if (nested_list_features_mobile.classList.contains('show-nested-list')) 
-    {
-        nested_list_features_mobile.classList.remove('show-nested-list');
-        uparrow1.classList.add('hide-icon');
-        downarrow1.classList.remove('hide-icon');
-    }
-    else
-    {
-        nested_list_features_mobile.classList.add('show-nested-list');
-        uparrow1.classList.remove('hide-icon');
-        downarrow1.classList.add('hide-icon');
-    }
-});
 
-toggle_company_mobile.addEventListener('click', () => {
-    if (nested_list_company_mobile.classList.contains('show-nested-list')) 
-    {
-        nested_list_company_mobile.classList.remove('show-nested-list');
-        uparrow2.classList.add('hide-icon');
-        downarrow2.classList.remove('hide-icon');
-    }
-    else
-    {
-        nested_list_company_mobile.classList.add('show-nested-list');
-        uparrow2.classList.remove('hide-icon');
-        downarrow2.classList.add('hide-icon');
-    }
-});
+//  Grab all nested links
+const nestedLinks = document.querySelectorAll('.nested-link');
 
-//  Toggle Nested list Desktop
+//  Add an event listener for each nested link for whn it is clicked
+nestedLinks.forEach(link => {
+    link.addEventListener('click',(e)=> {
+        let element = e.currentTarget;
+        const nestedList = element.querySelector('.nested-list');
+        const upArrow = element.querySelector('.up-arrow');
+        const downArrow = element.querySelector('.down-arrow');
+        if (nestedList.classList.contains('show-nested-list')){
+            upArrow.classList.add('hide-arrow');
+            downArrow.classList.remove('hide-arrow');
+            nestedList.classList.remove('show-nested-list');
+        }
+        else
+        {
+            upArrow.classList.remove('hide-arrow');
+            downArrow.classList.add('hide-arrow');
+            nestedList.classList.add('show-nested-list');
+        }
 
-toggle_features_desktop.addEventListener('click', () => {
-    if (nested_list_features_desktop.classList.contains('show-nested-list-features')) 
-    {
-        nested_list_features_desktop.classList.remove('show-nested-list-features');
-        updesktop1.classList.add('hide-icon');
-        downdesktop1.classList.remove('hide-icon');
-
-    }
-    else
-    {
-        nested_list_features_desktop.classList.add('show-nested-list-features');
-        updesktop1.classList.remove('hide-icon');
-        downdesktop1.classList.add('hide-icon');
-       
-    }
-});
-
-toggle_company_desktop.addEventListener('click', () => {
-    if (nested_list_company_desktop.classList.contains('show-nested-list-company')) 
-    {
-        nested_list_company_desktop.classList.remove('show-nested-list-company');
-        updesktop2.classList.add('hide-icon');
-        downdesktop2.classList.remove('hide-icon');
-
-    }
-    else
-    {
-        nested_list_company_desktop.classList.add('show-nested-list-company');
-        updesktop2.classList.remove('hide-icon');
-        downdesktop2.classList.add('hide-icon');
-        
-    }
-});
+    })
+})
